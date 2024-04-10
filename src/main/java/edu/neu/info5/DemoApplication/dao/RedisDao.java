309b6671a3d9f728200c72f69a4f02a58b18e774
divya-kharche <113252176+divya-kharche@users.noreply.github.com>
2024-04-10 14:11:42 -0400

    Dao creation and implemetation

---
+++ src/main/java/edu/neu/info5/DemoApplication/dao/RedisDao.java
@@ -0,0 +1,35 @@
+package edu.neu.info5.DemoApplication.dao;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public interface RedisDao {
+    public void postValue (String key, String value);
+    public boolean deleteValue(String key);
+    public Object getValue(String key);
+
+    void hSet(String key, String field, String value);
+
+    void hDelete(String key, String field);
+
+    void setAdd(String key, String value);
+
+    boolean hasKey(String key);
+
+    Set<String> keys(String keyPattern);
+
+    void deleteKey(String key);
+
+    void deleteKeys(Collection<String> keys);
+
+    Map<Object, Object> hGetAll(String key);
+
+    void hDelete(String key);
+
+    Set<String> sMembers(String key);
+
+    String hGet(String key, String field);
+
+
+}
