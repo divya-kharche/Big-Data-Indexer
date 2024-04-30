f5c49df7bf30b004d78a239755b761a150b4b264
divya-kharche <113252176+divya-kharche@users.noreply.github.com>
2024-04-30 14:14:07 -0400

    tests

---
+++ src/test/java/edu/neu/info5/DemoApplication/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.neu.info5.DemoApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
